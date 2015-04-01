ec08e81651264cf2df5f8aa5b2892bce8ca10c65
RoykoSerhiy <SerhiyRoiiko96@gmail.com>
2015-04-01 23:00:53 +0300

    no message

---
+++ TeamProject/TeamProject/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TeamPrj.Buisnes.Managers.Abstract;
+using TeamPrj.Buisnes.Managers.Concrete;
+using TeamProject.ViewModel;
+
+namespace TeamProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["TeamProjectEntities"].ConnectionString;
+        static public ICountryManager countryManager = new CountryManager(connectionString);
+        static public ICityManager cityManager = new CityManager(connectionString);
+        static public IResidenseManager residenseManager = new ResidenseManager(connectionString);
+        static public CountryViewModel countriesViewModel = new CountryViewModel(countryManager);
+        static public CityViewModel citiesViewModel = new CityViewModel(cityManager);
+        static public ResidenseViewModel residenseViewModel = new ResidenseViewModel(residenseManager);
+        public int CountryID;
+        public int CityID;
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+       
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+           try
+           {
+               cbCountry.DataContext = cmbCountryItems;
+               cbCountry.SelectedIndex = 0;
+              
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show(ex.Message);
+           }
+        }
+
+        private void cbCountry_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+           CountryID = countriesViewModel.Countries
+                .Where(c => c.Name == cbCountry.SelectedItem.ToString())
+                .Select(c => c.ID).First();
+
+           cbCity.DataContext = cmbCityItems;
+           cbCity.SelectedIndex = 0;
+          // MessageBox.Show(CountryID.ToString());
+        }
+        public IEnumerable<string> cmbCountryItems
+        {
+            get
+            {
+                return countriesViewModel.Countries.OrderBy(c => c.Name).Select(c => c.Name).ToList();
+            }
+
+        }
+        public IEnumerable<string> cmbCityItems
+        {
+            get
+            {
+                return citiesViewModel.Cities.Where(c => c.CountryId == CountryID).OrderBy(c => c.Name)
+                    .Select(c => c.Name).ToList();
+                //return citiesViewModel.Cities.OrderBy(c => c.Name).Select(c => c.Name).ToList();
+            }
+        }
+
+        private void cbCity_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+            try
+            {
+                CityID = citiesViewModel.Cities
+                    .Where(c => c.Name == cbCity.SelectedItem.ToString())
+                    .Select(c => c.ID).First();
+                //MessageBox.Show(CityID.ToString());
+                dgResidence.DataContext = residenseViewModel.Catalog.Where(r => r.CityId == CityID);
+            }
+            catch (Exception ex)
+            {
+                // MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
---
+++ TeamProject/TeamProject/ViewModel/ResidenseViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamPrj.Buisnes.Managers.Abstract;
+using EDModel;
+using TeamProject.Models;
+
+
+namespace TeamProject.ViewModel
+{
+    public class ResidenseViewModel
+    {
+        private readonly IResidenseManager _residenseManager;
+
+        List<ResidenseModel> _items = null;
+        List<Residense> _residenses = null;
+
+        public List<ResidenseModel> CreateResidenseModel(IEnumerable<Residense> residenses)
+        {
+            List<ResidenseModel> listCatalogModel = new List<ResidenseModel>();
+
+            var _res = from resid in residenses
+                       select new
+                       {
+                           Id = resid.ID,
+                           CityId = resid.CityId,
+                           Name = resid.Name,
+                           Adress = resid.Address,
+                           Raiting = resid.Raiting,
+                           Phone = resid.Phone,
+                           Price = resid.Price
+                       };
+            foreach (var r in _res)
+            {
+                listCatalogModel.Add(new ResidenseModel(r.Id , r.CityId , r.Name , r.Adress , r.Raiting , r.Phone ,r.Price));
+            }
+            return listCatalogModel;
+        }
+        public IEnumerable<ResidenseModel> Catalog
+        {
+            get
+            {
+                if (_items == null)
+                {
+                    _items = CreateResidenseModel(
+                           _residenseManager.GetAll()
+                        );
+                }
+                return _items;
+            }
+        }
+        public IEnumerable<Residense> Residenses
+        {
+            get
+            {
+                if (_residenses == null)
+                {
+                    return _residenseManager.GetAll();
+                }
+                return _residenses;
+            }
+        }
+        public ResidenseViewModel(
+                IResidenseManager residenseManager
+            )
+        {
+            _residenseManager = residenseManager;
+        }
+
+    }
+}
